50edbfb2e5cbe97bb08bd62c02f7803c6ebe3148
hermawanln <hermawan.luthfi@gmail.com>
2018-05-27 21:29:48 +0700

    add page /kilas-balik

---
+++ pages/kilas-balik.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    id="e3"
+    style="max-width: 400px; margin: auto;"
+    class="grey lighten-3"
+  >
+    <v-card>
+      <v-container
+        fluid
+        style="min-height: 0;"
+        grid-list-lg
+      >
+        <v-layout row wrap>
+          <v-flex xs12 left>
+            <v-card color="blue-grey darken-2" class="white--text">
+              <v-card-title primary-title>
+                <div class="headline">Unlimited music now</div>
+                <div>Listen to your favorite artists and albums whenever and wherever, online and offline.</div>
+              </v-card-title>
+              <v-card-actions>
+                <v-btn flat dark>Listen now</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+          <v-flex xs12>
+            <v-card color="black" class="white--text">
+              <v-container fluid grid-list-lg>
+                <v-layout row>
+                  <v-flex xs7>
+                    <div>
+                      <div class="headline">11 Mei 2011</div>
+                      <div>We are not talking about change, We are making change. </div>
+                    </div>
+                  </v-flex>
+                  <v-flex xs5>
+                    <v-card-media
+                      src="https://futureleadersummit.org/2017/assets/img/kilas/2011.png"
+                      height="125px"
+                      contain
+                    ></v-card-media>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card>
+          </v-flex>
+          <v-flex xs12>
+            <v-card color="purple" class="white--text">
+              <v-container fluid grid-list-lg>
+                <v-layout row>
+                  <v-flex xs7>
+                    <div>
+                      <div class="headline">Halycon Days</div>
+                      <div>Ellie Goulding</div>
+                    </div>
+                  </v-flex>
+                  <v-flex xs5>
+                    <v-card-media
+                      src="https://futureleadersummit.org/2017/assets/img/kilas/2011.png"
+                      height="125px"
+                      contain
+                    ></v-card-media>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+  </div>
+</template>
